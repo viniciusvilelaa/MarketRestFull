ddeaff998f8c20350fb8b83c3fb03dd9beadf6be
viniciusvilelaa <ninobez@icloud.com>
2025-01-05 18:45:13 -0300

    Adicicionando as permissoes no securityConfig

--- src/main/java/br/imd/Market/infra/Security/SecurityConfigurations.java
+++ src/main/java/br/imd/Market/infra/Security/SecurityConfigurations.java
@@ -32,12 +32,45 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+
+                        //ROLES USER
+                        .requestMatchers(HttpMethod.GET,"/produtos/getAll").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/produtos/getById/{id}").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/pedidos/getAll").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/pedidos/getById/{id}").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/clientes/getAll").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/clientes/getById/{id}").hasRole("USER")
+
+                        //ROLES ADMIN
                         .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/pedidos/postPedido").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE, "/pedidos/DeleteLogic/{id}").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT, "pedidos/putPedido/").hasRole("USER")
-                        .requestMatchers(HttpMethod.GET, "/pedidos/getById/").hasRole("USER")
-                        .anyRequest().hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/produtos/postProduto").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/produtos/DeleteLogic/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/produtos/DeleteProduto/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/produtos/putProduto/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/produtos/getAll").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/produtos/getById/{id}").hasRole("ADMIN")
+                        //PEDIDOS
+                        .requestMatchers(HttpMethod.POST, "/pedidos/postPedido").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/pedidos/putPedido/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/pedidos/DeleteLogic/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/pedidos/DeletePedido/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/pedidos/getAll/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/pedidos/getById/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/pedidos/AdicionarProduto/{Id}/{Id2}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/pedidos/RemoverProduto/{Id}/{Id2}").hasRole("ADMIN")
+
+                        //CLIENTE
+                        .requestMatchers(HttpMethod.POST, "/clientes/postCliente").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/clientes/putCliente/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/clientes/DeleteLogic/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/clientes/DeleteCliente/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/clientes/getAll/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/clientes/getById/{id}").hasRole("ADMIN")
+
+
+
+
+
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
